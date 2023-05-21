20e6f2f328aae9196c279e32eecbab20feec926a
Troy Harvey <harveydevel@gmail.com>
2023-05-21 15:37:20 +1000

    Add support for recursive file operations.

--- rmeta.sh
+++ rmeta.sh
@@ -1,32 +1,68 @@
 #!/bin/bash
-# Script requires: mkvtoolnix and kid3
+# Script requires: mkvtoolnix and kid3.
+
+# Non recursive operations for mkv.
+nr_mkv() {
+    for mkvname in *.mkv
+    do
+        mkvpropedit "$mkvname" -e info -s title="$(basename -s .mkv "$mkvname")"
+    done
+}
+
+# Recursive operations for mkv.
+r_mkv() {
+    shopt -s globstar
+    for mkvname in ./**/*.mkv
+    do
+        mkvpropedit "$mkvname" -e info -s title="$(basename -s .mkv "$mkvname")"
+    done
+}
+
+# Non recursive operations for mp4.
+nr_mp4() {
+    for mp4name in *.mp4
+    do
+        kid3-cli -c "set title '$(basename -s .mp4 "$mp4name")'" "$mp4name"
+        echo "Title metadata of $mp4name has been changed"
+    done
+}
+
+# Recursive operations for mp4.
+r_mp4() {
+    shopt -s globstar
+    for mp4name in ./**/*.mp4
+    do
+        kid3-cli -c "set title '$(basename -s .mp4 "$mp4name")'" "$mp4name"
+        echo "Title metadata of $mp4name has been changed"
+    done
+}
+
 
 case "$1" in
     mkv | --mkv )
-        for mkvname in *.mkv
-        do
-            mkvpropedit "$mkvname" -e info -s title="$(basename -s .mkv "$mkvname")"
-        done
+        if [[ $2 == "-r" ]] || [[ $2 == "--recursive" ]]; then
+            r_mkv
+        else
+            nr_mkv
+        fi
     ;;
 
     mp4 | --mp4 )
-        for mp4name in *.mp4
-        do
-            kid3-cli -c "set title '$(basename -s .mp4 "$mp4name")'" "$mp4name"
-            echo "Title metadata of $mp4name has been changed"
-        done
+        if [[ $2 == "-r" ]] || [[ $2 == "--recursive" ]]; then
+            r_mp4
+        else
+            nr_mp4
+        fi
     ;;
 
     all | --all )
-        for mkvname in *.mkv
-        do
-            mkvpropedit "$mkvname" -e info -s title="$(basename -s .mkv "$mkvname")"
-        done
-        for mp4name in *.mp4
-        do
-            kid3-cli -c "set title '$(basename -s .mp4 "$mp4name")'" "$mp4name"
-            echo "Title metadata of $mp4name has been changed"
-        done
+        if [[ $2 == "-r" ]] || [[ $2 == "--recursive" ]]; then
+            r_mkv
+            r_mp4
+        else
+            nr_mp4
+            nr_mkv
+        fi
     ;;
 
     * )
@@ -34,13 +70,18 @@ case "$1" in
         
         Usage: [command] [argument]
 
-        For mkv files:
+        For modifying all .mkv files in the current directory:
         rmeta mkv
         
-        For mp4 files:
+        For modifying all .mp4 files in the current directory:
         rmeta mp4
         
-        For both mkv and mp4 files:
-        rmeta all"
+        For both .mkv and .mp4 files in the current directory:
+        rmeta all
+
+        Append -r or --recursive to modify files under each directory recursively:
+        rmeta mkv -r
+        rmeta mp4 -r
+        rmeta all -r"
     ;;
 esac
